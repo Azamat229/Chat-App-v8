9bdf0ff3a5594643a6115191aaa00e246520702c
azamat <azamatmambetaliev229@gmail.com>
2021-05-02 18:43:18 +0600

    Completed lesson 4

--- app/src/main/java/com/example/chatapp_v8/NewMessageActivity.kt
+++ app/src/main/java/com/example/chatapp_v8/NewMessageActivity.kt
@@ -2,10 +2,19 @@ package com.example.chatapp_v8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CalendarView
+import com.example.chatapp_v8.model.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.user_row_new_message.view.*
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,19 +24,50 @@ class NewMessageActivity : AppCompatActivity() {
         supportActionBar?.title = "Select User"
 
 
-        val adapter = GroupAdapter<ViewHolder>()
-        recyclerview_newmessage.adapter = adapter
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
+//        val adapter = GroupAdapter<ViewHolder>()
+//        recyclerview_newmessage.adapter = adapter
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+
+        fetchUsers()
 
     }
 
+    private fun fetchUsers() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+                p0.children.forEach() {
+                    Log.d("New Message", it.toString())
+                    val user = it.getValue(User::class.java)
+                    if(user != null){
+                        adapter.add(UserItem(user))
+
+                    }
+                }
+
+                recyclerview_newmessage.adapter = adapter
+
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+//                TODO("Not yet implemented")
+            }
+
+
+        })
+
+    }
 }
 
-class UserItem : Item<ViewHolder>() {
+class UserItem(private val user:User) : Item<ViewHolder>() { // Private
     override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.username_textview_new_message.text = user.username
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.imageview_new_message)
 
     }
 
